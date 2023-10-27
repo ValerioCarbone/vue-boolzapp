e680e1e4c6cca2e313b73b61a99a12fafdf2cd44
ValerioCarbone <valeriocarbone10@gmail.com>
2023-10-27 10:55:31 +0200

    ADD - Scroll contacts

--- js/app.js
+++ js/app.js
@@ -165,7 +165,8 @@ createApp({
                             status: 'received'
                         }
                     ],
-                }
+                },
+                
             ],
             chatIndex: 0,
             contactSearch: '',
